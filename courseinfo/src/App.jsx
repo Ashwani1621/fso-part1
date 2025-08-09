b5a90b5f32085607cd95ee4dd2c05f4582a8519e
Ashwani Singh <itsashksingh10@gmail.com>
2025-08-09 16:59:32 +0530

    1.1 Done

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -47,8 +47,10 @@ const App = () => {
       <Content
         part1 = {part1}
         part2 = {part2} 
+        part3 = {part3}
         exercises1 = {exercises1} 
         exercises2 = {exercises2}
+        exercises3 = {exercises3}
       />
       <Total 
         exercises1 = {exercises1}
